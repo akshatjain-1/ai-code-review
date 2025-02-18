d02eda1b7f0b097ae1bcad3e96d1ef109d9e60bf
Akshat Jain <jnakshat202@gmail.com>
2025-02-19 02:14:37 +0530

    front

---
+++ backend/src/main/java/com/example/codereview/controller/GithubWebhookController.java
@@ -0,0 +1,10 @@
+package com.example.codereview.controller;
+
+import com.example.codereview.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class GithubWebhookController {
+    
+}
